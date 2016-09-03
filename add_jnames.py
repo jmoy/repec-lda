98df3074a498f6a95b1ee759003562e501416902
Jyotirmoy Bhattacharya <jyotirmoy@jyotirmoy.net>
2016-09-03 15:51:01 +0530

    Add a rank to journals. Lower is better.

--- add_jnames.py
+++ add_jnames.py
@@ -3,31 +3,31 @@ import sqlite3
 import sys
 
 archives = [
-    ("oup:qjecon", "Quarterly Journal of Economics"),
-    ("aea:aecrev", "American Economic Review"),
-    ("aea:aejmac", "American Economic Journal: Macroeconomics"),
-    ("bla:ijethy", "International Journal of Economic Theory"),
-    ("bla:jeurec", "Journal of European Economic Association"),
-    ("bla:restud", "Review of Economic Studies"),
-    ("ecm:emetrp", "Econometrica"),
-    ("eee:dyncon", "Journal of Economic Dynamics and Control"),
-    ("eee:gamebe", "Games and Economic Behaviour"),
-    ("eee:jbfina", "Journal of Banking and Finance"),
-    ("eee:jetheo", "Journal of Economic Theory"),
-    ("eee:moneco", "Journal of Monetary Economics"),
-    ("mcb:jmoncb", "Journal of Money, Credit and Banking"),
-    ("oup:restud", "Review of Economic Studies"),
-    ("red:issued", "Review of Economic Dynamics"),
-    ("spr:joecth", "Economic Theory"),
-    ("spr:jogath", "International Journal of Game Theory"),
-    ("the:publsh", "Theoretical Economics"),
-    ("tpr:jeurec", "Journal of European Economic Association"),
-    ("tpr:qjecon", "Quarterly Journal of Economics"),
-    ("tpr:restat", "Review of Economics and Statistics"),
-    ("ucp:macann", "NBER Macroeconomics Annual"),
-    ("wly:emetrp", "Econometrica"),
-    ("wly:jmoncb", "Journal of Money, Credit and Banking"),
-    ("eee:matsoc", "Mathematical Social Sciences")
+    ("oup:qjecon", "Quarterly Journal of Economics",2),
+    ("aea:aecrev", "American Economic Review",1),
+    ("aea:aejmac", "American Economic Journal: Macroeconomics",3),
+    ("bla:ijethy", "International Journal of Economic Theory",4),
+    ("bla:jeurec", "Journal of European Economic Association",4),
+    ("bla:restud", "Review of Economic Studies",1),
+    ("ecm:emetrp", "Econometrica",1),
+    ("eee:dyncon", "Journal of Economic Dynamics and Control",3),
+    ("eee:gamebe", "Games and Economic Behaviour",3),
+    ("eee:jbfina", "Journal of Banking and Finance",3),
+    ("eee:jetheo", "Journal of Economic Theory",2),
+    ("eee:moneco", "Journal of Monetary Economics",3),
+    ("mcb:jmoncb", "Journal of Money, Credit and Banking",3),
+    ("oup:restud", "Review of Economic Studies",3),
+    ("red:issued", "Review of Economic Dynamics",3),
+    ("spr:joecth", "Economic Theory",4),
+    ("spr:jogath", "International Journal of Game Theory",4),
+    ("the:publsh", "Theoretical Economics",2),
+    ("tpr:jeurec", "Journal of European Economic Association",4),
+    ("tpr:qjecon", "Quarterly Journal of Economics",2),
+    ("tpr:restat", "Review of Economics and Statistics",2),
+    ("ucp:macann", "NBER Macroeconomics Annual",3),
+    ("wly:emetrp", "Econometrica",1),
+    ("wly:jmoncb", "Journal of Money, Credit and Banking",3),
+    ("eee:matsoc", "Mathematical Social Sciences",4)
 ]
 
 
@@ -35,7 +35,7 @@ if __name__=="__main__":
   dbcon = sqlite3.connect(sys.argv[1])
   dbcon.execute("DROP TABLE IF EXISTS series")
   dbcon.execute("""CREATE TABLE series
-                        (seriesid TEXT PRIMARY KEY, name TEXT)""")
-  dbcon.executemany("INSERT INTO series VALUES (?,?)",archives)
+                        (seriesid TEXT PRIMARY KEY, name TEXT,rank SMALLINT)""")
+  dbcon.executemany("INSERT INTO series VALUES (?,?,?)",archives)
   dbcon.commit()
   dbcon.close()
